fb8d5d8d0e62b1d5939c5a34878b6285aecb67f5
Zek Yang <zekai_yang@berkeley.edu>
2025-09-18 18:28:16 -0700

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,93 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+
+#    Git is the language, GitHub is the online repository, and Git Bash is an app for Windows users to use Git.
+
+# 2) What’s the difference between the terminal and the command line?
+
+#    The terminal is the application, the command line is the interface where we can edit our text.
+
+# 3) How does Windows PowerShell differ from Git Bash?
+
+#    Windows PowerShell is developed for Windows environments, integrated with the Windows OS. Git Bash allows Windows users to use git operations and other features.
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+
+#    Python is a general-purpose programming language.
+#    Anaconda is a distribution of Python and R, specifically tailored for data science, machine learning, and scientific computing.
+#    Conda is a system package manager which is designed to install, run, and update packages and their dependencies.
+
+# 5) What is VS Code?
+
+#    VS Code is a source code editor. 
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+
+#    Jupyter Notebook is a web-based interface for creating and sharing documents that contains code, while JupyterLab is a more advanced workspace that also contains Jupyter Notebook.
+
+# 7) What does ~/ mean?
+
+#    It represents the user's hoome directory.
+
+# 8) What’s the difference between an absolute path and a relative path?
+
+#    Absolute path is the full location of the file from the root directory, relative path is the location of the file from the current directory.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+
+#    Absolute: /Users/zekyang/python_decal_fa25/course_assignments/homework2
+#    Relative: ../course_assignments/homework2
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+
+#     cd ..
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+
+#     It removes the directory.
+
+# 12) What do the following commands do?
+# git add
+# git commit
+# git push
+#     git add will add our file to the staging area to allow git to track the files.
+#     git commit saves our files which are being tracked in the staging area to the local repository.
+#     git push tells our local repository to save the changes to our remote repository as well.
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+
+#     git add . adds all available files, while git add <file> adds only the specified file.
+
+# 14) What do "git status" and "git log -1" do?
+
+#     git status shows the current state of my  working directory and staging area in Git
+#     git log -1 shows only the latest commit
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+
+#     Cloning creates a local copy of a remote repository.
+#     Pulling from a repository merges the latest updates or changes from the repository into a local copy.
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+
+#     I haven't run into many but there was once when I was unable to run any git commands in the terminal, which was really annoying. 
+#     Luckily, one of the instructors helped me out by running "exit<>" and that fixed the issue.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+
+#     I don't really know how some of the commands work (like the one above), but I will figure it out with time.
+
+# 18) Tell me a fun fact!
+
+#     Kazakhstan's capital holds the Guinness World Record for the capital city with the most name changes in modern times!
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+
+print(1 + 1) #this adds the numbers, a fundamental mathematical concept which is critical to modern life as we know it. How cool is that!
+# (Hint: Use print() and add a comment explaining what it does.)
